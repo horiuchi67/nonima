47d07f0e18bb53648ad796a8451031b2974a18d7
horiuchi67 <1234mhoriuchi@gmail.com>
2020-07-02 12:04:46 +0900

    ジャンル機能を修正しました。また、カスタマーとアドミンでコントローラー、モデル、ビューを分けました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :customers
   resources :customers,only: [:show,:edit,:update]
   get '/customers/:id/leave' => "customers#leave"
@@ -13,5 +14,13 @@ Rails.application.routes.draw do
   post '/orders/complete' => "orders#create"
   resources :orders,only: [:new,:create,:index,:show]
   root to: 'home#top'
+  namespace :admin do
+  	get '/' => "home#top"
+  	resources :items,only: [:index,:new,:create,:show,:edit,:update]
+  	resources :genres,only: [:index,:create,:edit,:update]
+  	resources :customers,only: [:index,:show,:edit,:update]
+  	resources :orders,only: [:index,:show,:update]
+  	resources :order_details,only: [:update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
